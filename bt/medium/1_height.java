3526702f63c71e06983e93393f795b9e411122c4
Priyam-Mondal <mondalpriyam99@gmail.com>
2024-03-23 08:04:12 +0700

    Add new file: 1_height.java

--- bt/medium/1_height.java
+++ bt/medium/1_height.java
@@ -1,21 +1,3 @@
-/************************************************************
-
- Following is the TreeNode class structure
-
- class TreeNode {
-     int data;
-     TreeNode left;
-     TreeNode right;
-
-     TreeNode(int data) {
-         this.data = data;
-         this.left = null;
-         this.right = null;
-     }
- }
-
- ************************************************************/
-
  public class Solution {
     public static int heightOfBinaryTree(TreeNode root) {
         if(root==null){
