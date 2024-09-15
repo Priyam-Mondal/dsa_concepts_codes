4cadf403e9c9082e650eafbb89255d4b23a0959c
Priyam Mondal <mondalpriyam99@gmail.com>
2024-09-15 09:09:21 +0700

    first commit

--- string/easy/2_check_palindrome.java
+++ string/easy/2_check_palindrome.java
@@ -1,29 +1,23 @@
-/*
-
-Given an unsorted integer array, find a pair with the given sum in it.
-
-• Each input can have multiple solutions. The output should match with either one of them.
-
-Input : nums[] = [8, 7, 2, 5, 3, 1], target = 10
-Output: (8, 2) or (7, 3)
-
-• The solution can return pair in any order. If no pair with the given sum exists, the solution should return null.
-
-Input : nums[] = [5, 2, 6, 8, 1, 9], target = 12
-Output: null
-
-*/
-
-class Solution
-{
-	/* The Pair<U, V> class have
-		1. Two member variables, first and second.
-		2. Factory method `Pair.of(U, V)` for creating its immutable instance.
-		3. equals() and hashCode() methods overridden.
-	*/
-
-	public static Pair<Integer, Integer> findPair(int[] nums, int target)
-	{
-		// Write your code here...
-	}
+class Solution {
+    public boolean isPalindrome(String s) {
+        int n= s.length();
+        int i= 0;
+        int j= n-1;
+        while(i <= j){
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {//moving start until alphanumeric char found.
+                i++;
+            }
+            
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {//moving end for same as above.
+                j--;
+            }
+            int c1= Character.toLowerCase(s.charAt(i));
+            int c2= Character.toLowerCase(s.charAt(j));
+            if(c1 != c2){
+                return false;
+            }
+            i++; j--;
+        }
+        return true;
+    }
 }
