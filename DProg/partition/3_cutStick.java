8d3beef6d48f49ed2a347682a3f2cfe5896ff5d7
Priyam Mondal <mondalpriyam99@gmail.com>
2024-09-17 14:09:35 +0700

    partition dp

---
+++ DProg/partition/3_cutStick.java
@@ -0,0 +1,67 @@
+/*Given a wooden stick of length n units. The stick is labelled from 0 to n.
+For example, a stick of length 6 is labelled as follows:
+
+Given an integer array cuts where cuts[i] denotes a position you should perform a cut at.
+You should perform the cuts in order, you can change the order of the cuts as you wish.
+The cost of one cut is the length of the stick to be cut, 
+the total cost is the sum of costs of all cuts. When you cut a stick, 
+it will be split into two smaller sticks (i.e. the sum of their lengths is 
+the length of the stick before the cut). Please refer to the first example for a better explanation.
+
+Return the minimum total cost of the cuts.
+
+Example 1:
+
+
+Input: n = 7, cuts = [1,3,4,5]
+Output: 16
+Explanation: Using cuts order = [1, 3, 4, 5] as in the input leads to the following scenario:
+
+The first cut is done to a rod of length 7 so the cost is 7.
+The second cut is done to a rod of length 6 (i.e. the second part of the first cut), 
+the third is done to a rod of length 4 and the last cut is to a rod of length 3. 
+The total cost is 7 + 6 + 4 + 3 = 20.
+Rearranging the cuts to be [3, 5, 1, 4] for example will lead to a 
+scenario with total cost = 16 (as shown in the example photo 7 + 4 + 3 + 2 = 16).
+Example 2:
+
+Input: n = 9, cuts = [5,6,1,4,2]
+Output: 22
+Explanation: If you try the given cuts ordering the cost will be 25.
+There are much ordering with total cost <= 25, for example,
+the order [4, 6, 5, 2, 1] has total cost = 22 which is the minimum possible. */
+
+
+
+class Solution {
+    public int minCost(int n, int[] cuts) {
+        // Adding 0 at start and n at end to the cuts array
+        int[] c = new int[cuts.length + 2];
+        c[0] = 0;
+        for (int i = 0; i < cuts.length; i++) {
+            c[i + 1] = cuts[i];
+        }
+        c[c.length - 1] = n;
+
+        // Sort the cuts array so that left and right subproblems are independent
+        Arrays.sort(c);
+
+        int[][] dp = new int[c.length][c.length];
+
+        // Fill the dp table in a bottom-up manner
+        for (int i = c.length - 2; i >= 1; i--) {
+            for (int j = i; j <= c.length - 2; j++) {
+                int mini = Integer.MAX_VALUE;
+                // Try cutting at each position between i and j
+                for (int ind = i; ind <= j; ind++) {
+                    int cost = c[j + 1] - c[i - 1] + dp[i][ind - 1] + dp[ind + 1][j];
+                    mini = Math.min(mini, cost);
+                }
+                dp[i][j] = mini;
+            }
+        }
+
+        // The final answer is the cost of cutting the entire rod from 0 to n
+        return dp[1][c.length - 2];
+    }
+}
